388b74a842e05c21972955ad4034c2999a80af75
JosemGomezGarzon <142318478+JosemGomezGarzon@users.noreply.github.com>
2023-08-15 14:58:38 +0000

    testRun

---
+++ TestJava/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    String color = "dos";
+    
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+            
+    }
+}
